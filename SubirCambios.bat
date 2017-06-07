1451a9fcfb13bbad9831ee5300b6bc1854b080cd
Arriola Mario <arriola.mario.90@gmail.com>
2017-06-07 20:22:27 -0300

    20170607202227

---
+++ SubirCambios.bat
@@ -0,0 +1,15 @@
+echo off
+rem e:
+rem cd e:\Proyectos\Laboratorio\ApVenta
+rem git status
+Set Fecha=%Date:~6,4%%Date:~3,2%%Date:~0,2%
+Set Hora=%Time:~0,2%%Time:~3,2%%Time:~6,2%
+git add .
+
+git commit -m %Fecha%%Hora%
+
+git pull
+
+git push
+
+exit
