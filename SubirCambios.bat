f0fc14d5545f94e05e5dfcc4178084593abdee9e
Arriola Mario <arriola.mario.90@gmail.com>
2017-06-20 01:52:39 -0300

    20170620 15239

--- SubirCambios.bat
+++ SubirCambios.bat
@@ -1,9 +1,17 @@
 echo off
 Set Fecha=%Date:~6,4%%Date:~3,2%%Date:~0,2%
 Set Hora=%Time:~0,2%%Time:~3,2%%Time:~6,2%
+echo "Ejecutando ADD.."
+echo ""
 git add .
+echo "Ejecutando commit.."
+echo ""
 git commit -m "%Fecha%%Hora%"
+echo "Ejecutando pull.."
+echo ""
 git pull
+echo "Ejecutando push.."
+echo ""
 git push
 
 pause
