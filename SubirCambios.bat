48731119058e9eb18cecdca425ebe042dd01d051
Arriola Mario <arriola.mario.90@gmail.com>
2017-06-20 01:49:32 -0300

    20170620 14929

--- SubirCambios.bat
+++ SubirCambios.bat
@@ -1,15 +1,15 @@
 echo off
-rem e:
-rem cd e:\Proyectos\Laboratorio\ApVenta
-rem git status
 Set Fecha=%Date:~6,4%%Date:~3,2%%Date:~0,2%
 Set Hora=%Time:~0,2%%Time:~3,2%%Time:~6,2%
+echo %Fecha%
+echo %Hora%
+pause
 git add .
-
-git commit -m %Fecha%%Hora%
-
+pause
+git commit -m "%Fecha%%Hora%"
+pause
 git pull
-
+pause
 git push
 
 pause
